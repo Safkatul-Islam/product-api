0f120ddaf05282ec77e081e28cb042848ea9fcff
Md Safkatul Islam <mrsafkatul.stu24@gmail.com>
2025-12-02 01:10:51 -0800

    Add HomeController with welcome message for the Product API

---
+++ src/main/java/com/myportfolio/product_api/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.myportfolio.product_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the Product API! Access the data at /api/products";
+    }
+}
